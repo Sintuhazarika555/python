704fc68428e83a69e295a4f1528314d8b3729df1
Sintu Hazarika <sintuhazarika555@gmail.com>
2025-03-18 11:19:15 +0530

    .

---
+++ lab1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+def add(a, b):
+    return a + b
+
+result = add(a, b)
+print(f"The sum of {a} and {b} is {result}")
